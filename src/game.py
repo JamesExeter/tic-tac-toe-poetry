f785ba3781bb8d2aae7fc104cf79c90ffb527b48
JamesExeter <jb75426@gmail.com>
2021-10-07 15:18:32 +0100

    initial commit

---
+++ src/game.py
@@ -0,0 +1,34 @@
+PLAYER_SYMBOLS = {
+    1: 'X',
+    2: 'O'
+}
+
+BOARD = [
+    [" ", " ", " "],
+    [" ", " ", " "],
+    [" ", " ", " "]
+]
+
+def check_win(board):
+    pass
+
+def check_valid_move(x, y):
+    pass
+
+def print_board(board):
+    pass
+
+def update_board(new_board):
+    pass
+
+def make_move(symbol):
+    pass
+
+def move(symbol):
+    pass
+
+def play_game():
+    pass
+
+if __name__ == "__main__":
+    pass
